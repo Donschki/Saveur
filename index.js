2efd6b5ceba4f2e8b234f0df1e9b37d866df4ae2
donschki <donvonkrog@gmail.com>
2022-12-05 14:14:03 +0200

    a

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 
 const hamburger = document.querySelector(".hamburger-menu");
 const navList = document.querySelector('.nav-list');
+const contact = document.querySelector('.contact');
 
 hamburger.addEventListener('click', () => {
     navList.classList.toggle ('change');
+
 });
+
+//                          CLICK TO REMOVE SIDE BAR
+
+ document.querySelector('.nav-list').addEventListener('click', () => {
+    navList.classList.remove('change')
+ });
+
+ document.querySelector('main').addEventListener('click', () => {
+    navList.classList.remove('change');
+  });
+  
+  document.querySelector('footer').addEventListener('click', () => {
+    navList.classList.remove('change');
+  });
 
 
 
